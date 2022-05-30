2e552637e88498d4276d9730ae1912f1b71ba9a0
BMarquez96 <marquezbrian.96@gmail.com>
2022-05-30 12:37:00 -0300

    Cambiado el metodo del form de post a GET, para evitar errores

--- scripts/validacion_form_contacto.js
+++ scripts/validacion_form_contacto.js
@@ -1,8 +1,8 @@
-function enviarForm(){
     const nombreF = document.getElementById('nombre').value;
     const emailF = document.getElementById('email').value;
     const asuntoF = document.getElementById('asunto').value;
-       
+
+function enviarForm(){         
     if(nombreF === '' || emailF === '' || asuntoF === ''){
         alert('Por favor revisa los datos, faltan completar campos obligatorios.')
     }

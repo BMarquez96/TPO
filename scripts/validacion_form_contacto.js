236bfcd85851cabb21614307dce6e0a3a86e3d2c
BMarquez96 <marquezbrian.96@gmail.com>
2022-05-30 14:24:26 -0300

    Corregido error de validacion JS

--- scripts/validacion_form_contacto.js
+++ scripts/validacion_form_contacto.js
@@ -1,9 +1,10 @@
+function enviarForm(){     
     const nombreF = document.getElementById('nombre').value;
     const emailF = document.getElementById('email').value;
     const asuntoF = document.getElementById('asunto').value;
-
-function enviarForm(){         
+        
     if(nombreF === '' || emailF === '' || asuntoF === ''){
         alert('Por favor revisa los datos, faltan completar campos obligatorios.')
+        
     }
 };
